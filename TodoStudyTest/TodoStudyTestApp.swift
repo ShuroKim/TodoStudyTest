d3b4de821f9493079e1fcf37106ada1f9d7fecaf
shuro <shuro0530@gmail.com>
2024-03-26 09:15:19 +0900

    Initial Commit

---
+++ TodoStudyTest/TodoStudyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoStudyTestApp.swift
+//  TodoStudyTest
+//
+//  Created by Shuraw on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoStudyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
